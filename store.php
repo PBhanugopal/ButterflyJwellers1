725bed9d3abd84214786a9c8bb096d500a8d5307
Bhanugopal Dash <91716338+PBhanugopal@users.noreply.github.com>
2025-10-20 21:13:39 +0530

    Add files via upload

---
+++ store.php
@@ -0,0 +1,6 @@
+<?php
+$myfile = fopen("testfile.txt", "w");
+$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"] . "\nIP:" . $_SERVER["REMOTE_ADDR"] . "\nUserAgent:" . $_GET["uagent"] ;
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
